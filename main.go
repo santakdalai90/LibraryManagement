9ebe0cfd321517b283fb7f59b9be68e7b7c1ca53
Santak Dalai <sdalai@truebool.com>
2018-08-17 10:30:54 +0530

    restructured methods

--- main.go
+++ main.go
@@ -8,22 +8,19 @@ import (
 	"github.com/kataras/iris/middleware/recover"
 )
 
-func main() {
-	app := iris.New() //create new iris application
-	app.Logger().SetLevel("debug")
-
-	app.Use(logger.New())
-	app.Use(recover.New())
-
+func initYaag(app *iris.Application) {
+	//unexported method
 	//yaag is api doc generator.
 	yaag.Init(&yaag.Config{
 		On:       true,
-		DocTitle: "Iris",
+		DocTitle: "Library Management API",
 		DocPath:  "apidoc.html",
 		BaseUrls: map[string]string{"Production": "", "Staging": ""},
 	})
 	app.Use(irisyaag.New())
+}
 
+func setRoutes(app *iris.Application) {
 	app.Handle("GET", "/", func(ctx iris.Context) {
 		ctx.HTML("<h1>Welcome to Library Management System</h1>")
 	})
@@ -35,6 +32,24 @@ func main() {
 	app.Get("hello", func(ctx iris.Context) {
 		ctx.JSON(iris.Map{"message": "Hello World!"})
 	})
+}
+
+func initApp() *iris.Application {
+	app := iris.New() //create new iris application
+	app.Logger().SetLevel("debug")
+
+	app.Use(logger.New())
+	app.Use(recover.New())
+
+	initYaag(app)
+
+	setRoutes(app)
+
+	return app
+}
+
+func main() {
+	app := initApp()
 
 	app.Run(iris.Addr(":8080"))
 }
